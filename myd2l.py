8d122f04dcdeb386ebaca7023d8d55e9c514ca07
numan947 <mahmudulhasan947@gmail.com>
2019-10-19 16:27:49 +0600

    Added more functionalities in myd2l

--- myd2l.py
+++ myd2l.py
@@ -5,8 +5,11 @@ from IPython import display
 
 
 ## PYTORCH
+import torch
+import torch.utils.data as data
 
-import 
+import torchvision
+from torchvision.transforms import transforms
 
 
 
@@ -108,9 +111,98 @@ def synthetic_data(w, b, num_examples):
     return X,y
 
 
+def load_array(data_arrays, batch_size, is_train=True):
+    """Construct a Pytorch data loader--> data_arrays must be (TensorX, TensorY)"""
+    dataset = data.TensorDataset(*(data_arrays))
+    return data.DataLoader(dataset,batch_size=batch_size, shuffle=is_train)
+
+
+def get_dataloader_workers(num_workers=4):
+    if sys.platform.startswith("win"):
+        return 0
+    else:
+        return num_workers
+
+
+def accuracy(y_hat, y):
+    """Accuracy for Softmax Regression --> y_hat is one_hotted, y is not, both are Tensor, returns number of correctly classified examples"""
+    return (y_hat.argmax(axis=1) == y).sum().item()
+
+
+def evaluate_accuracy(net, data_iter, device=torch.device('cpu')):
+    """Mainly for Softmax Regression"""
+    net.eval() #change to evaluation mode
+    acc_sum, n = torch.tensor([0], dtype=torch.float32, device=device),0
+
+    for X,y in data_iter:
+        X,y = X.to(device), y.to(device)
+        with torch.no_grad():
+            y = y.long()
+            acc_sum+=torch.sum((torch.argmax(net(X), dim=1) == y))
+            n+=y.shape[0]
+    return acc_sum.item()/n
+
+
+def evaluate_loss(net, data_iter, criterion):
+    """Evaluate the loss of a model on the given dataset"""
+
+    metric = Accumulator(2)
+    for X,y in data_iter:
+        metric.add(criterion(net(X),y).sum().detach().numpy().item(), list(y.shape)[0])
+    return metric[0]/metric[1]
 
 
 
+class Accumulator(object):
+    """Sum a list of numbers over time"""
+    def __init__(self, n):
+        self.data = [0.0] * n
+    def add(self, *args):
+        self.data = [a+b for a, b in zip(self.data, args)]
+    def reset(self):
+        self.data = [0] * len(self.data)
+    def __getitem__(self, i):
+        return self.data[i]
+
+def train_ch3(net, train_iter, test_iter, criterion, num_epochs, batch_size, lr=None):
+    """Train and evaluate a model with CPU"""
+    optimizer = torch.optim.SGD(net.parameters(), lr=lr)
+
+    for epoch in range(num_epochs):
+        sum_loss, sum_acc, n = 0.0,0.0,0
+        for X, y in train_iter:
+            optimizer.zero_grad()
+            y_hat = net(X)
+            loss = criterion(y_hat, y)
+            loss.backward()
+            optimizer.step()
+
+            y = y.type(torch.float32)
+            sum_loss+=loss.item()
+            sum_acc+=torch.sum((torch.argmax(y_hat, dim=1).type(torch.FloatTensor) == y).detach()).float()
+            n+=list(y.size())[0]
+        test_acc = evaluate_accuracy(net, test_iter)
+
+        print("Epoch {}, loss {}, train acc {}, evaluation acc {}".format(epoch+1, sum_loss/n, sum_acc/n, test_acc))
 
 
+
+##################### FASHION_MNIST###############################
+def get_fashion_mnist_labels(labels):
+    text_labels = ['t-shirt', 'trouser', 'pullover', 'dress', 'coat',
+                   'sandal', 'shirt', 'sneaker', 'bag', 'ankle boot']
+    return [text_labels[int(i)] for i in labels]
+
+def load_data_fashion_mnist(batch_size, resize=None):
+    tx = None
+    if resize:
+        tx = transforms.Compose([transforms.Resize(resize)])
     
+    fashionTrain = torchvision.datasets.FashionMNIST("./data/FashionMnist", train=True, transforms=tx, download=True)
+    fashionTest = torchvision.datasets.FashionMNIST("./data/FashionMnist", train=False, transforms=tx, download=True)
+
+    trainLoader = data.DataLoader(fashionTrain, num_workers=get_dataloader_workers(4), batch_size=batch_size, shuffle=True)
+    testLoader = data.DataLoader(fashionTest, num_workers=get_dataloader_workers(4), batch_size=batch_size, shuffle=False)
+
+    return (trainLoader, testLoader)
+
